48e46a08058361b68c5902cf41c5ddf6b720ca85
Olaf Alders <olaf@wundersolutions.com>
2016-04-05 18:32:34 -0400

    Adds more compat layers and more tests.

--- lib/UserAgent/Compat.pm
+++ lib/UserAgent/Compat.pm
@@ -2,6 +2,8 @@ use strict;
 use warnings;
 package UserAgent::Compat;
 
+use Scalar::Util qw( blessed );
+
 sub new {
     my $class = shift;
     my %args  = @_;
@@ -12,34 +14,52 @@ sub new {
 
 sub delete {
     my $self = shift;
-    return $self->_res('delete', @_ );
+    return $self->_res( 'delete', @_ );
 }
 
 sub get {
     my $self = shift;
-    return $self->_res('get', @_ );
+    return $self->_res( 'get', @_ );
 }
 
 sub head {
     my $self = shift;
-    return $self->_res('head', @_ );
+    return $self->_res( 'head', @_ );
 }
 
 sub post {
     my $self = shift;
-    return $self->_res('post', @_ );
+    return $self->_res( 'post', @_ );
 }
 
 sub put {
     my $self = shift;
-    return $self->_res('put', @_ );
+    return $self->_res( 'put', @_ );
+}
+
+sub request {
+    my $self = shift;
+    my $req  = shift;
+    my $res;
+
+    if ( $self->{user_agent}->isa('HTTP::Tiny') ) {
+        my @args = (
+            $req->uri, $req->method,
+            { headers => $req->headers->flatten || [], content => $req->content }
+        );
+        $res = $self->{user_agent}->request(@args);
+    }
+    else {
+        $res = $self->{user_agent}->request($req);
+    }
+    return UserAgent::Compat::Response->new($res);
 }
 
 sub _res {
-    my $self = shift;
+    my $self   = shift;
     my $method = shift;
-    my $res = $self->{user_agent}->$method( @_ );
-    return UserAgent::Compat::Response->new( $res );
+    my $res    = $self->{user_agent}->$method(@_);
+    return UserAgent::Compat::Response->new($res);
 }
 
 1;
@@ -58,36 +78,67 @@ use Scalar::Util qw( blessed );
 
 sub new {
     my $class = shift;
-    my $res = shift;
+    my $res   = shift;
 
     die 'You must provide a response object' unless $res;
     return bless { raw => $res }, $class;
 }
 
+sub code {
+    my $self = shift;
+    unless ( blessed( $self->{raw} ) ) {
+        return $self->{raw}->{status};
+    }
+
+    if ( $self->{raw}->isa('Mojo::Transaction::HTTP') ) {
+        return $self->{raw}->res->code;
+    }
+
+    return $self->{raw}->code;
+}
+
+sub content {
+    my $self = shift;
+    unless ( blessed( $self->{raw} ) ) {
+        return $self->{raw}->{content};
+    }
+
+    if ( $self->{raw}->isa('Mojo::Transaction::HTTP') ) {
+        return $self->{raw}->res->body;
+    }
+
+    return $self->{raw}->content;
+}
+
+sub as_object {
+    my $self = shift;
+    return $self->{raw};
+}
+
 sub headers {
     my $self = shift;
-    my $raw = $self->{raw};
+    my $raw  = $self->{raw};
 
     # HTTP::Tiny returns a HashRef
-    unless ( blessed( $raw ) ) {
+    unless ( blessed($raw) ) {
         return $raw->{headers};
     }
 
-    if ( $raw->isa('Mojo::Transaction::HTTP' )) {
+    if ( $raw->isa('Mojo::Transaction::HTTP') ) {
         return $raw->res->headers->to_hash;
     }
 
     if ( $raw->can('headers') ) {
-        return [$raw->headers->flatten];
+        return [ $raw->headers->flatten ];
     }
 }
 
 sub is_success {
     my $self = shift;
-    my $raw = $self->{raw};
+    my $raw  = $self->{raw};
 
     # HTTP::Tiny returns a HashRef
-    unless ( blessed( $raw ) ) {
+    unless ( blessed($raw) ) {
         return $raw->{success};
     }
 
@@ -97,9 +148,8 @@ sub is_success {
     }
 
     # Mojo::Transaction::HTTP
-    return substr($raw->res->code, 0, 1) == 2;
+    return substr( $raw->res->code, 0, 1 ) == 2;
 }
 
 1;
-
 
--- t/compat.t
+++ t/compat.t
@@ -3,7 +3,9 @@ use Test::More;
 use UserAgent::Compat;
 
 use DDP;
+use Devel::Confess;
 use Furl;
+use HTTP::Request;
 use HTTP::Tiny;
 use LWP::UserAgent;
 use Mojo::UserAgent;
@@ -22,17 +24,35 @@ foreach my $class (@ua_classes) {
     my $compat = UserAgent::Compat->new( user_agent => $class->new );
     ok( $compat, 'compiles' );
 
-    my $res;
-    try {
-        $res = $compat->get('http://wundercharts.com');
+METHOD:
+    foreach my $method ( 'delete', 'get', 'head', 'patch', 'post', 'put' ) {
+        my $res;
+        my $url = 'http://127.0.0.1:3000';
+        diag '-'x40;
+        try {
+            $res
+                = $method ne 'patch'
+                ? $compat->$method($url)
+                : $compat->request( HTTP::Request->new( PATCH => $url ) );
+        }
+        catch {
+            diag $_;
+            #exit;
+            next METHOD;
+        };
+        ok( $res,             "$method via " . $class );
+        ok( $res->is_success, 'is_success' );
+        ok( $res->headers,    'headers' );
+        ok( $res->code,       'code: ' . $res->code );
+        unless ( $method eq 'head' ) {
+            diag( np( $res ) );
+            ok( $res->content, 'content' );
+        }
+        #diag( np( $res->headers ) );
+
+        #    diag( np( $res->as_object ) );
     }
-    catch {
-        diag $_
-    };
-    ok( $res, 'get via ' . $class );
-    ok( $res->is_success, 'is_success' );
-    ok( $res->headers, 'headers' );
-    diag( np( $res->headers ) );
 }
 
 done_testing();
+
